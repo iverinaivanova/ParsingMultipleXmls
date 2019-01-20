b9675a20f2a6d6e4e7f7557525e9e2838ad9f39d
Iva <44803236+iverinaivanova@users.noreply.github.com>
2019-01-20 22:21:32 +0100

    Update parsing_multiple_xmls.py

--- parsing_multiple_xmls.py
+++ parsing_multiple_xmls.py
@@ -1,8 +1,9 @@
 '''
 The following script parses all xml files in a directory and counts
-the length of the abstracts and the bodytext of the articles, as well as the number of self mentions 
-(i.e. 1st sg and pl personal pronouns). Then it appends the filename, 
-the lengths of the abstracts and the body of the articles, and the count of self mentions to a csv file.
+the length of the abstracts and the bodytext of the articles, 
+as well as the number of self mentions (i.e. 1st sg and pl personal pronouns). 
+Then it appends the filename, the lengths of the abstracts and the body of the articles, 
+and the count of self mentions to a csv file.
 
 '''
 import os
