fafadd10842bf400eba8f351541fec19c2761e28
Iva <44803236+iverinaivanova@users.noreply.github.com>
2019-01-20 22:20:16 +0100

    Update parsing_multiple_xmls.py

--- parsing_multiple_xmls.py
+++ parsing_multiple_xmls.py
@@ -1,7 +1,8 @@
 '''
 The following script parses all xml files in a directory and counts
-the length of the abstracts and the bodytext of the articles. Then it appends the filename and 
-the lengths of the abstracts and the body of the articles to a csv file.
+the length of the abstracts and the bodytext of the articles, as well as the number of self mentions 
+(i.e. 1st sg and pl personal pronouns). Then it appends the filename, 
+the lengths of the abstracts and the body of the articles, and the count of self mentions to a csv file.
 
 '''
 import os
@@ -16,23 +17,33 @@ for filename in os.listdir(path):
     '''for elem in root.iter():
         print(elem.tag, elem.attrib)'''
     lengths = []
+    texts = []
+    pronouns = []
+    selfMent = ["we", "our", "us", "i", "me", "my"]
     title = root[0][0][0].text
     print(title)
     for bodyText in root.iter('bodyText'):
         abstract = str(bodyText.text)
         words = abstract.split()
         lengths.append(len(words))
+        texts.append(words)
     for fullname in range(1):
         abst = lengths[0]
         body = sum(lengths[1:len(lengths)])
         print("Abstract length: ", abst)
         print("Body length: ", body)
+        abstMent = texts[0]
+        #bodyMent = texts[1:len(texts)]
+        all_small = str(abstMent).lower()
+        for word in selfMent[:]:
+            if word in all_small:
+                pronouns.append(word)
+        selfMent_num = len(pronouns)  
         myFile = "../myFile_2.csv"
-        csvRow = [filename, abst, body]
+        csvRow = [filename, abst, body, selfMent_num]
         with open(myFile, 'a') as f:
             writer = csv.writer(f, dialect = "excel")
             writer.writerow(csvRow)
-            writer.writerow(['filename', 'abst', 'body'])
         print("Writing complete!")
         
         
